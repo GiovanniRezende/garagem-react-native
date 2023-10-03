6e66037441fb484d4b1f7c3f6d812515d0ad0d99
GiovanniRezende <Giovanni.wifc@gmail.com>
2023-10-03 14:57:59 -0300

    HomeScreen feita

---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+
+// body
+
+<h1>Titulo</h1>
+
+const styles = StyleSheet.create({
+
+// styles
+
+});
+
+
+export default HomeScreen;
